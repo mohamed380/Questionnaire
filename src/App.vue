21915c7b33b4b26f40d57df627f730c0577a415a
Mohamed Atia <m.atia@easternenterprise.com>
2022-06-20 13:28:00 +0200

    -Main layout style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
   <div class="container-fluied px-4">
+    <NavBar />
     <RouterView />
   </div>
 </template>
